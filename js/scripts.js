3759687294b59cf71a9e08df415d16a30952401a
futomakiyoin <futomakiyoin@gmail.com>
2021-05-19 13:51:41 -0400

    Working Search Bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
-fetch(
-  "https://raw.githubusercontent.com/futomakiyoin/pcoptimum_html/main/json/offers.json"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log("error: " + err);
+// Most of the code of https://www.webslesson.info/2017/02/live-search-json-data-using-ajax-jquery.html
+$(document).ready(function () {
+  $.ajaxSetup({ cache: false });
+  $("#search").keyup(function () {
+    $("#offers").html("");
+    var searchField = $("#search").val();
+    var expression = new RegExp(searchField, "i");
+    $.getJSON(
+      "https://raw.githubusercontent.com/futomakiyoin/pcoptimum_html/main/json/offers.json",
+      function (data) {
+        $.each(data, function (key, value) {
+          if (value.Offers.search(expression) != -1) {
+            $("#offers").append(
+              '<div class="mb-2"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
+                value.Get +
+                ' points</p><p class="card-text"> For every ' +
+                value.Buy +
+                'spend on <span class="fw-bold">' +
+                value.Offers +
+                "</span></p></div></div></div>"
+            );
+          }
+        });
+      }
+    );
   });
-function appendData(data) {
-  var mainContainer = document.getElementById("offers");
-  for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML =
-      '<div class="mb-2"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
-      data[i].Get +
-      ' points</p><p class="card-text"> For every ' +
-      data[i].Buy +
-      'spend on <span class="fw-bold">' +
-      data[i].Offers +
-      "</span></p></div></div></div>";
-    mainContainer.appendChild(div);
-  }
-}
+
+  $("#offers").on("click", "li", function () {
+    var click_text = $(this).text().split("|");
+    $("#search").val($.trim(click_text[0]));
+    $("#offers").html("");
+  });
+});
