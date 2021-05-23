ca12b61b18bd95a14b0a96b78f1713b0cd2a02a1
futomakiyoin <futomakiyoin@gmail.com>
2021-05-23 01:25:46 -0400

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 // LOAD ALL CONTENT AT LAUNCH
+// Most of the code of https://www.webslesson.info/2017/02/live-search-json-data-using-ajax-jquery.html
 $.getJSON(
   "https://raw.githubusercontent.com/futomakiyoin/pcoptimum_html/main/json/offers.json",
   function (data) {
+    // for each data use function
     $.each(data, function (key, value) {
+      // if Buy does not exist
       if (typeof value.Buy == "undefined") {
+        // add html code in offers
         $("#offers").append(
           '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
-            value.Get +
-            ' points</p><p class="card-text"><span class="fw-bold">' +
-            value.Offers +
-            "</span></p></div></div></div>"
+          value.Get +
+          ' points</p><p class="card-text"><span class="fw-bold">' +
+          value.Offers +
+          "</span></p></div></div></div>"
         );
-      } else {
+      }
+      // if Buy does exist
+      else {
+        // add html code in offers
         $("#offers").append(
           '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
-            value.Get +
-            ' points</p><p class="card-text"> For every ' +
-            value.Buy +
-            ' spend on <span class="fw-bold">' +
-            value.Offers +
-            "</span></p></div></div></div>"
+          value.Get +
+          ' points</p><p class="card-text"> For every ' +
+          value.Buy +
+          ' spend on <span class="fw-bold">' +
+          value.Offers +
+          "</span></p></div></div></div>"
         );
       }
     });
@@ -27,36 +34,44 @@ $.getJSON(
 );
 
 // SEARCH FUNCTION
-// Most of the code of https://www.webslesson.info/2017/02/live-search-json-data-using-ajax-jquery.html
-
 $(document).ready(function () {
-  $.ajaxSetup({ cache: false });
+  $.ajaxSetup({
+    cache: false
+  });
+  // check content in search
   $("#search").keyup(function () {
-    $("#offers").html(""); // erase id offers
+    // erase content in offers
+    $("#offers").html("");
+    // save search content as variable
     var searchField = $("#search").val();
     var expression = new RegExp(searchField, "i");
     $.getJSON(
       "https://raw.githubusercontent.com/futomakiyoin/pcoptimum_html/main/json/offers.json",
       function (data) {
+        // for each data use function
         $.each(data, function (key, value) {
           if (value.Offers.search(expression) != -1) {
             if (typeof value.Buy == "undefined") {
+              // add html code in offers
               $("#offers").append(
                 '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
-                  value.Get +
-                  ' points</p><p class="card-text"><span class="fw-bold">' +
-                  value.Offers +
-                  "</span></p></div></div></div>"
+                value.Get +
+                ' points</p><p class="card-text"><span class="fw-bold">' +
+                value.Offers +
+                "</span></p></div></div></div>"
               );
-            } else {
+            }
+            // if Buy does exist
+            else {
+              // add html code in offers
               $("#offers").append(
                 '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
-                  value.Get +
-                  ' points</p><p class="card-text"> For every ' +
-                  value.Buy +
-                  ' spend on <span class="fw-bold">' +
-                  value.Offers +
-                  "</span></p></div></div></div>"
+                value.Get +
+                ' points</p><p class="card-text"> For every ' +
+                value.Buy +
+                ' spend on <span class="fw-bold">' +
+                value.Offers +
+                "</span></p></div></div></div>"
               );
             }
           }
