9eed52153223fb28cecb3ed1751b5c54e5f0dff2
futomakiyoin <futomakiyoin@gmail.com>
2021-06-02 22:33:47 -0400

    Fix search blinking issue

--- js/scripts.js
+++ js/scripts.js
@@ -41,10 +41,10 @@ $(document).ready(function () {
   // check content in search
   $("#search").keyup(function () {
     // erase content in offers
-    $("#offers").html("");
     // save search content as variable
     var searchField = $("#search").val();
     var expression = new RegExp(searchField, "i");
+    var offerList = "";
     $.getJSON(
       "https://raw.githubusercontent.com/futomakiyoin/pcoptimum_html/main/json/offers.json",
       function (data) {
@@ -53,29 +53,28 @@ $(document).ready(function () {
           if (value.Offers.search(expression) != -1) {
             if (typeof value.Buy == "undefined") {
               // add html code in offers
-              $("#offers").append(
+              offerList +=
                 '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
                 value.Get +
                 ' points</p><p class="card-text"><span class="fw-bold">' +
                 value.Offers +
-                "</span></p></div></div></div>"
-              );
+                "</span></p></div></div></div>";
             }
             // if Buy does exist
             else {
               // add html code in offers
-              $("#offers").append(
+              offerList +=
                 '<div class="pt-3"><div class="card" style="height:7rem;"><div class="card-body"><p class="card-title fw-bold text-orange">' +
                 value.Get +
                 ' points</p><p class="card-text"> For every ' +
                 value.Buy +
                 ' spend on <span class="fw-bold">' +
                 value.Offers +
-                "</span></p></div></div></div>"
-              );
+                "</span></p></div></div></div>";
             }
           }
         });
+        $("#offers").html(offerList);
       }
     );
   });
